293ba92d6abf153c427288bc1db1c2255e405fbb
Paramount22 <jurica.marek@gmail.com>
2021-03-14 09:25:04 +0100

    vote

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts');
 Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
+/*LIKE | UNLIKE*/
 Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
 Route::post('/posts/{post}/unlikes', [PostUnlikeController::class, 'store'])->name('posts.unlikes');
 
